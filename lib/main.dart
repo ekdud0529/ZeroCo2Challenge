83fb4b85615e322b5af5de7dcf9c5d5054a01b48
daykim <45174177+ekdud0529@users.noreply.github.com>
2022-09-07 15:57:54 +0900

    Add Login/SignUp
    1. 파이어베이스 연동
    2. 로그인 및 회원가입 기능 구현
    3. 로그인 성공시 메인 페이지 이동 구현
    
    Add

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:zeroco/pages/navpages/main_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutterfire_ui/auth.dart';
 
-void main() {
+void main() async{
+  // 플러터에서 firebase 사용을 위해선 메인메소드 내에서 비동기 방식으로 반드시
+  // 아래 메서드 호출한 후 firebase.initial~을 불러와야함
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      home: MainPage()
-
+      home: accountPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
+class accountPage extends StatelessWidget{
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Authentication(),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class Authentication extends StatelessWidget {
+  const Authentication({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot){
+        if (!snapshot.hasData){
+          return SignInScreen(
+            providerConfigs: [
+              EmailProviderConfiguration()
+            ],
+          );
+        }
+        return MainPage();
+      },
     );
   }
 }
