ab0b040e2f46e28b0f140755f5c7d97072e1affe
narin111 <jungnr11@naver.com>
2022-09-13 22:06:39 +0900

    Add tableCalendar 1. 캘린더 추가 2. 선택한 날짜로 DB 저장

--- lib/pages/navpages/home_page.dart
+++ lib/pages/navpages/home_page.dart
@@ -25,96 +25,192 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  TextEditingController _eventController = TextEditingController();
+  late Map<DateTime, List<Event>> selectedEvents;
+  CalendarFormat format = CalendarFormat.month;
+  DateTime selectedDay = DateTime.now();
+  DateTime focusedDay = DateTime.now();
   DatabaseReference ref = FirebaseDatabase.instance.ref("performance");
-  String name="";
+
+  TextEditingController _eventController = TextEditingController(); // 텍스트 필드에서 text 가져오기
+  String name = "";
+
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("challenge"),
-        ),
-        body: Center(
-          child: FutureBuilder<List<Challenge>>(
-            future: DatabaseHelper.instance.getChallenge(),
-            builder: (BuildContext context,
-                AsyncSnapshot<List<Challenge>> snapshot){
-              if(!snapshot.hasData){
-                return Center(child:Text('Loading'));
-              }
-              return snapshot.data!.isEmpty
-                ? Center(child:Text('챌린지에 참여해보세요!'))
-                : ListView( // 챌린지 리스트
-                  children: snapshot.data!.map((challenge){
-                    return Center(
-                      child:ListTile(
-                        title:Text(challenge.challenge),
-                        // title : Column(
-                        //   children: [
-                        //     const Text(challenge.challenge),
-                        //   ],
-                        // ),
-                      ),
-                    );
-                  }).toList(),
-                );
-            }),
-        ),
-        floatingActionButton: FloatingActionButton.extended(
-          onPressed: () => showDialog(
-            context: context,
-            builder: (context) => AlertDialog(
-              title: Text("Today's Challenge"),
-              content: TextFormField(
-                controller: _eventController,
+  void initState() {
+    selectedEvents = {};
+    super.initState();
+  }
+
+  List<Event> _getEventsfromDay(DateTime date) {
+    return selectedEvents[date] ?? [];
+  }
+
+
+  @override
+  void dispose() {
+    _eventController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("CO2 Challenge"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          TableCalendar(
+            focusedDay: selectedDay,
+            firstDay: DateTime(1990),
+            lastDay: DateTime(2050),
+            calendarFormat: format,
+            onFormatChanged: (CalendarFormat _format) {
+              setState(() {
+                format = _format;
+              });
+            },
+            startingDayOfWeek: StartingDayOfWeek.sunday,
+            daysOfWeekVisible: true,
+
+            //Day Changed
+            onDaySelected: (DateTime selectDay, DateTime focusDay) {
+              setState(() {
+                selectedDay = selectDay;
+                focusedDay = focusDay;
+              });
+              print(focusedDay);
+            },
+            selectedDayPredicate: (DateTime date) {
+              return isSameDay(selectedDay, date);
+            },
+
+            eventLoader: _getEventsfromDay,
+
+            //To style the Calendar
+            calendarStyle: CalendarStyle(
+              isTodayHighlighted: true,
+              selectedDecoration: BoxDecoration(
+                color: Colors.blue,
+                // color: (255, 188, 80),
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(10.0),
               ),
-              actions: [
-                TextButton(
-                  child: Text("Cancel"),
-                  onPressed: () => Navigator.pop(context),
-                ),
-                TextButton(
-                  child: Text("Ok"),
-                  onPressed: () async {
-                    print("push btn");
-                    // 텍스트 필드가 비었을 때
-                    if (_eventController.text.isEmpty) {
-
-                    } else { // challenge 입력
-                      // 로그인한 사용자정보
-                      User? user = FirebaseAuth.instance.currentUser;
-                      for(final providerProfile in user!.providerData){
-                        final emailAddress = providerProfile.email;
-                        List<String>? currentUser = emailAddress?.split('@');
-                        name = currentUser![0];
-                      }
-                      // 현재 날짜
-                      // final dateStr = DateFormat('yyyy-MM-dd').format(selectedDay);
-                      await ref
-                          .child(name)
-                          .set(_eventController.text)
-                          .asStream();
-                      // sqlite
-                      await DatabaseHelper.instance.add(
-                        Challenge(date:"2022-09-12", challenge: _eventController.text),
-                      );
-                    }
-                    Navigator.pop(context);
-                    _eventController.clear();
-
-                    setState((){});
-
-                    return;
-                  },
-                ),
-              ],
+              selectedTextStyle: TextStyle(color: Colors.white),
+              todayDecoration: BoxDecoration(
+                color: Colors.purpleAccent,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              defaultDecoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              weekendDecoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+            headerStyle: HeaderStyle(
+              formatButtonVisible: true,
+              titleCentered: true,
+              formatButtonShowsNext: false,
+              formatButtonDecoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              formatButtonTextStyle: TextStyle(
+                color: Colors.white,
+              ),
             ),
           ),
-          label: Text("Add Challenge"),
-          icon: Icon(Icons.add),
+          /// 새로추가
+          Expanded(
+            child:FutureBuilder<List<Challenge>>(
+                future: DatabaseHelper.instance.getChallenge(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<List<Challenge>> snapshot){
+                  if(!snapshot.hasData){
+                    return Center(child:Text('Loading'));
+                  }
+
+                  return snapshot.data!.isEmpty
+                      ? Center(child:Text('챌린지에 참여해보세요!'))
+                      : ListView( // 챌린지 리스트
+                    children: snapshot.data!.map((challenge){
+                      return Center(
+                        child:ListTile(
+                          title:Text(challenge.challenge),
+
+                        ),
+                      );
+                    }).toList(),
+                  );
+                }),
+          ),
+
+          ///
+          ..._getEventsfromDay(selectedDay).map(
+                (Event event) => ListTile(
+              title: Text(event.title),
+            ),
+          ),
+        ],
+      ),
+
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text("Today's Challenge"),
+            content: TextFormField(
+              controller: _eventController,
+            ),
+            actions: [
+              TextButton(
+                child: Text("Cancel"),
+                onPressed: () => Navigator.pop(context),
+              ),
+              TextButton(
+                child: Text("Ok"),
+                onPressed: () async {
+                  print("push btn");
+                  // 텍스트 필드가 비었을 때
+                  if (_eventController.text.isEmpty) {
+
+                  } else { // challenge 입력
+                    // 로그인한 사용자정보
+                    User? user = FirebaseAuth.instance.currentUser;
+                    for(final providerProfile in user!.providerData){
+                      final emailAddress = providerProfile.email;
+                      List<String>? currentUser = emailAddress?.split('@');
+                      name = currentUser![0];
+                    }
+                    // 현재 날짜
+                    final dateStr = DateFormat('yyyy-MM-dd').format(selectedDay);
+                    await ref
+                        .child(name)
+                        .set(_eventController.text)
+                        .asStream();
+                    // sqlite
+                    await DatabaseHelper.instance.add(
+                      Challenge(date:dateStr, challenge: _eventController.text),
+                    );
+                  }
+                  Navigator.pop(context);
+                  _eventController.clear();
+
+                  setState((){});
+
+                  return;
+                },
+              ),
+            ],
+          ),
         ),
-      )
+        label: Text("Add Challenge"),
+        icon: Icon(Icons.add),
+      ),
     );
   }
 }
@@ -180,7 +276,6 @@ class DatabaseHelper{
     Database db = await instance.database;
     return await db.insert('my_challenge', challenge.toMap());
   }
-
 
 }
 
